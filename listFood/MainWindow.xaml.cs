72069a83e4404593c37c9fc6edebb72779575d72
tqxkn1112 <ntq20030807@gmail.com>
2020-05-28 11:54:23 +0700

    Re-push 2

--- listFood/MainWindow.xaml.cs
+++ listFood/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 namespace listFood
 {
     /// <summary>
@@ -45,6 +46,7 @@ namespace listFood
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             List<Food> listFoods = new List<Food>();
+            using Excel = Microsoft.Office.Interop.Excel;
             Excel.Application xlApp = new Excel.Application();
             Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"sandbox_test.xlsx");
             Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
